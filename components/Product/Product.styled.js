928776068afae376ce1474d09001887e23cbdc1f
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2023-11-07 11:37:40 +0100

    challenge done

--- components/Product/Product.styled.js
+++ components/Product/Product.styled.js
@@ -4,3 +4,11 @@ export const ProductCard = styled.article`
   padding: 0.5rem 1rem;
   box-shadow: 0px 1px 5px -2px var(--color-granite);
 `;
+
+export const ToolButton = styled.button`
+  padding: 0.3rem 0.5rem;
+  margin-block: 0.5rem;
+  margin-inline-end: 0.5rem;
+  border: none;
+  box-shadow: 0px 1px 5px -2px var(--color-granite);
+`;
--- components/Product/index.js
+++ components/Product/index.js
@@ -3,12 +3,50 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+import { ToolButton } from "./Product.styled";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    } else {
+      console.log(response.status);
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      await response.json();
+      router.push("/");
+    } else {
+      console.log(response.status);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -19,14 +57,25 @@ export default function Product() {
   }
 
   return (
-    <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
-      <StyledLink href="/">Back to all</StyledLink>
-    </ProductCard>
+    <>
+      <ToolButton type="button" onClick={() => setIsEditMode(!isEditMode)}>
+        ✏️
+      </ToolButton>
+      <ToolButton type="button" onClick={() => handleDeleteProduct(id)}>
+        🗑️
+      </ToolButton>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct}>Edit Fish</ProductForm>
+      )}
+      <ProductCard>
+        <h2>{data.name}</h2>
+        <p>Description: {data.description}</p>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
+        {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+        <StyledLink href="/">Back to all</StyledLink>
+      </ProductCard>
+    </>
   );
 }
